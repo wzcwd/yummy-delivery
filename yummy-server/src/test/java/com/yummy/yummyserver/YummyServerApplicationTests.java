34d0fc118bde30fae64be90b710d3e618aff4ebd
wzc <wzcvictory@gmail.com>
2025-01-19 15:12:00 -0800

    Commit a new project yummy-delivery

---
+++ yummy-server/src/test/java/com/yummy/yummyserver/YummyServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yummy.yummyserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YummyServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
