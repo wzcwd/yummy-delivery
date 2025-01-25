86c94c53d11b09b1bb2ef48e8f13eeb797446cd5
wzc <wzcvictory@gmail.com>
2025-01-25 01:49:26 -0800

    new update

---
+++ yummy-server/src/main/java/com/yummy/controller/admin/CategoryController.java
@@ -0,0 +1,94 @@
+package com.yummy.controller.admin;
+
+import com.yummy.dto.CategoryDTO;
+import com.yummy.dto.CategoryPageQueryDTO;
+import com.yummy.entity.Category;
+import com.yummy.result.PageResult;
+import com.yummy.result.Result;
+import com.yummy.service.CategoryService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+/**
+ * Category Management
+ */
+@RestController
+@RequestMapping("/admin/category")
+@Slf4j
+public class CategoryController {
+
+    @Autowired
+    private CategoryService categoryService;
+
+    /**
+     * add new category
+     * @param categoryDTO
+     * @return
+     */
+    @PostMapping
+    public Result<String> save(@RequestBody CategoryDTO categoryDTO){
+        log.info("add new category：{}", categoryDTO);
+        categoryService.save(categoryDTO);
+        return Result.success();
+    }
+
+    /**
+     * page query
+     * @param categoryPageQueryDTO
+     * @return
+     */
+    @GetMapping("/page")
+    public Result<PageResult> page(CategoryPageQueryDTO categoryPageQueryDTO){
+        log.info("page query：{}", categoryPageQueryDTO);
+        PageResult pageResult = categoryService.pageQuery(categoryPageQueryDTO);
+        return Result.success(pageResult);
+    }
+
+    /**
+     * delete category
+     * @param id
+     * @return
+     */
+    @DeleteMapping
+    public Result<String> deleteById(Long id){
+        log.info("delete category：{}", id);
+        categoryService.deleteById(id);
+        return Result.success();
+    }
+
+    /**
+     * update category
+     * @param categoryDTO
+     * @return
+     */
+    @PutMapping
+    public Result<String> update(@RequestBody CategoryDTO categoryDTO){
+        categoryService.update(categoryDTO);
+        return Result.success();
+    }
+
+    /**
+     * enable or disable category
+     * @param status
+     * @param id
+     * @return
+     */
+    @PostMapping("/status/{status}")
+    public Result<String> startOrStop(@PathVariable("status") Integer status, Long id){
+        categoryService.startOrStop(status,id);
+        return Result.success();
+    }
+
+    /**
+     * search category by type
+     * @param type
+     * @return
+     */
+    @GetMapping("/list")
+    public Result<List<Category>> list(Integer type){
+        List<Category> list = categoryService.list(type);
+        return Result.success(list);
+    }
+}
